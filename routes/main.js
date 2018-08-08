b3dd18d8d19d092a4fc377059601398f220dac98
Anthony Sin <anthony.t.sin@gmail.com>
2018-08-07 23:26:03 -0400

    cnn scraper

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const cnnScraper = require('../controllers/cnnScraper');
 
 router.get('/', (req, res) => {
   res.render('index');
+});
+
+router.get('/cnnscraper', (req, res) => {
+  cnnScraper.getCnnPage(req, res)
 });
 
 module.exports = router;
