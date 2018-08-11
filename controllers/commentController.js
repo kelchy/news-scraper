f3540dfb6d3e15d861b51a734ccfb414dcfb07d5
Anthony Sin <anthony.t.sin@gmail.com>
2018-08-11 12:06:30 -0400

    deleting comment now also removes the id in assoc article

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -3,7 +3,7 @@ const db = require('../models');
 const saveComment = (req, res) => {
   db.Comment.create(req.body)
     .then(comment => {
-      db.Article.findOneAndUpdate({ _id: req.params.article_id }, {$push: {"comments": comment._id}}, { new: true }).then(article => {
+      db.Article.findOneAndUpdate({ _id: req.params.article_id }, {$push: {comments: comment._id}}, { new: true }).then(article => {
         res.json([
           comment,
           article
@@ -18,9 +18,15 @@ const saveComment = (req, res) => {
 }
 
 const deleteComment = (req, res) => {
-  // bugged -> deleting comment but not reference
   db.Comment.deleteOne({_id: req.params.id}).then(deleted => {
-    res.json(deleted);
+    db.Article.findOneAndUpdate({_id: req.params.article_id}, {$pull: {comments: req.params.id}}, {new: true}).then(article => {
+      res.json([
+        deleted,
+        article
+      ]);
+    }).catch(err => {
+      res.json(err);
+    });
   }).catch(err => {
     res.json(err);
   });;
