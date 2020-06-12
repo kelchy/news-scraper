959ea52952b869e2d4594f8e4a21d7ea8345dcb9
Kelvin Chua <kelchy@gmail.com>
2020-06-12 11:00:04 +0800

    paginate

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -263,12 +263,10 @@ const Render = (() => {
     if (tabSelector == '#saved-tab') {
       const up = articles[0]._id;
       const down = articles[articles.length-1]._id;
-      $tdPrev = $('<td>', {html: `<i class="medium material-icons"><a href=# class="prev-next" data-id=${up} data-asc=1>chevron_left</a></i><i class="small"></i>`});
-      $tdNext = $('<td>', {html: `<i class="small"></i><i class="medium material-icons"><a href=# class="prev-next" data-id=${down} data-asc=0>chevron_right</a></i>`});
+      $tdPrev = $('<td>', {html: `<i class="medium material-icons"><a href=# class="prev-next" data-id=${up} data-asc=1>chevron_left</a></i><i class="small" style="vertical-align:middle"></i>`});
+      $tdNext = $('<td>', {html: `<i class="small" style="vertical-align:middle"></i><i class="medium material-icons"><a href=# class="prev-next" data-id=${down} data-asc=0>chevron_right</a></i>`});
       $tdNext.addClass('right-align');
       $tdNext.attr('colspan','3');
-      $tdNext.css('vertical-align', 'middle');
-      $tdPrev.css('vertical-align', 'middle');
       const $tr = $('<tr>').append($tdPrev, $tdNext);
       $('#saved-tab table.articles-table tbody').append($tr);
       $.ajax({
